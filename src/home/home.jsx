0eb43dcab2a4f934fc742721208fb0e8f096783e
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-08-31 22:03:39 -0400

    Initial commit

---
+++ src/home/home.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+export default function Home() {
+   
+    const [name, setName] = useState();
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link to="/todo">APP</Link>
+                <p>{name}</p>
+                <input type="text" onChange={(e)=> 
+                {setName(e.target.value)}}/>
+        </div>
+
+    );
+}

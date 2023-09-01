0498e26cb70e476acec50273c5289013e3f828ab
belaSimoes <isabelasimoes205@gmail.com>
2023-09-01 17:15:26 -0400

    css

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import './home.css';
+
 export default function Home() {
    
     const [name, setName] = useState();
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,31 +1,34 @@
+// Todo.js
+
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import './estilos.css'
 
 export default function Todo() {
-
   const [lista, setLista] = useState([]);
   const [id, setid] = useState(1);
   const [Atividade, setAtividade] = useState("");
-
 
   function salvar(e) {
     e.preventDefault();
     setLista([...lista, { Atividade: Atividade, id: id }]);
     setid(id + 1);
   }
-
-
   return (
-    <div>
-      <h1>todo</h1>
-
-      <Link to="/home">App</Link>
+    <div className="container">
+      <h1>TODO</h1>
+      <Link to="/home" className="button-link">App</Link> {/* Aplicar a classe "button-link" aqui */}
       <form onSubmit={salvar}>
-        <input onChange={(e) => setAtividade(e.target.value)} type="text" />
-        <button>add</button>
+        <div className="input-container">
+          <input
+            className="pink-border-input"
+            onChange={(e) => setAtividade(e.target.value)}
+            type="text"
+          />
+          <button className="pink-border-button">add</button>
+        </div>
       </form>
-      
+
       {lista.map((atividade) => (
         <div key={atividade.id}>
           <p>{atividade.id}</p>
